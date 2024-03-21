ccbcf844b6f6742774e2d26afa13241411d90c94
Baptiste-micl <163674826+Baptiste-micl@users.noreply.github.com>
2024-03-21 13:09:33 +0100

    modification base de donnée

--- base_de_donnée (officiel).sql
+++ base_de_donnée (officiel).sql
@@ -61,12 +61,55 @@ CREATE TABLE Resultats (
 
 
 -- Insertion pour les Pays
-INSERT INTO Pays
-VALUES	(1, 'FRANCE', 'https://fr.wikipedia.org/wiki/Drapeau_de_la_France#/media/Fichier:Flag_of_France_(1794%E2%80%931815,_1830%E2%80%931974).svg'),
-	(2, 'ALLEMAGNE', 'https://fr.wikipedia.org/wiki/Drapeau_de_l%27Allemagne#/media/Fichier:Flag_of_Germany.svg');
+INSERT INTO Pays (nom_pays, drapeau_pays)
+VALUES ('France', '[Image du drapeau français: URL non valide]'),
+       ('États-Unis', '[Image du drapeau américain: URL non valide]'),
+       ('Canada', '[Image du drapeau canadien: URL non valide]'),
+       ('Angleterre', '[Image du drapeau britannique: URL non valide]'),
+       ('Italie', '[Image du drapeau italien: URL non valide]');
 
 
 -- Insertion pour les disciplines
-INSERT INTO Pays
-VALUES	(1, 'Foot', 'Un jeu avec un ballon'),
-	(2, 'Judo', 'a quand gary vs kenzi ?');
+INSERT INTO Disciplines (nom_discipline, description_discipline)
+VALUES ('Athlétisme', 'Course, saut, lancer'),
+       ('Natation', 'Crawl, dos, papillon...'),
+       ('Judo', 'un jeu de bagarre');
+
+-- Insertion pour les sportifs
+INSERT INTO Sportifs (nom_sportif, prenom_sportif, date_de_naissance, id_pays, id_discipline, id_equipe)
+VALUES ('Mayer', 'Kevin', '1992-02-10', 1, 1, 1),
+       ('Riner', 'Teddy', '1989-04-07', 1, 3, 2),
+       ('Lyles', 'Noah', '1997-07-18', 2, 1, 3),
+       ('Phelps', 'Michael', '1985-06-30', 2, 2, 4),
+       ('Warner', 'Damian', '1989-10-04', 3, 1, 5),
+       ('Know', 'Finlay', '2001-01-08', 3, 2, 6)
+       ('Peaty', 'Adam', '1994-12-28', 4, 2, 7),
+       ('Clark', 'Sarah', '1978-01-03', 4, 3, 8),
+       ('Ceccon', 'Thomas', '2001-01-27', 5, 2, 9),
+       ('Basile', 'Fabio', '1994-09-07', 5, 3, 10);
+
+
+-- Insertion pour les equipes
+INSERT INTO Equipes (nom_equipe, id_pays, id_discipline, id_sportif)
+VALUES ('equipe de France d athlétisme','1','1','1')
+       ('equipe de France de judo', '1', '3', '2')
+       ('equipe des états-unis d athlétisme', '2', '1', '3')
+       ('equipe des état-unis de natation', '2', '2', '4')
+       ('equipe du canada d athlétisme')
+       ('equipe du canada de natation')
+       ('equipe d Angleterre de natation')
+       ('equipe d Angleterre de judo')
+       ('equipe d Italie de natation')
+       ('equipe d Italie de judo')
+
+
+-- Insertion pour les resultats
+INSERT INTO Resultats (id_pays, id_discipline, medaille_or, medaille_argent, medaille_bronze)
+VALUES (2, 2, 1, 0, 0),  -- USA, Natation, Or
+       (1, 1, 1, 0, 0),  -- France, Athlétisme, Or
+       (3, 1, 0, 1, 0),  -- Canada, Athlétisme, Argent
+       (1, 6, 1, 0, 0),  -- France, Tennis, Or
+       (2, 6, 1, 0, 0),  -- USA, Tennis, Or
+       (3, 6, 0, 1, 0),  -- Canada, Tennis, Argent
+       (1, 8, 0, 0, 1),  -- France, Gymnastique, Bronze
+       (7, 4, 1, 0, 0);  -- Espagne, Football, Or
