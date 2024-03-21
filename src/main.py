8de766c7c5859ecfc0398a3be6a77776954fbffb
Baptiste-micl <163674826+Baptiste-micl@users.noreply.github.com>
2024-03-21 22:59:46 +0100

    test

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ import tkinter as tk
 fenetre= tk.Tk()
 fenetre.title("Jeux-Olympiques")
 
-liste_resultat=["Pays", "Pays + discipline"]
+liste_resultat = Listbox(fenetre)
+liste_resultat.insert(1, "test")
 
 # Création d'un bouton
 bouton = tk.Button(fenetre, text="Quitter", command=fenetre.quit)
