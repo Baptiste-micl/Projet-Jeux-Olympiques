a061e05d8a51093b06ca7704856caf7c1a214600
Skymut <152205908+Skymut@users.noreply.github.com>
2024-03-24 11:48:17 +0100

    Update main.py
    changement du menu de droite (non opérationnel)

--- src/main.py
+++ src/main.py
@@ -18,15 +18,30 @@ listes = ["Sportifs", "Disciplines"]
 # Ici on charge l'image
 fond = tk.PhotoImage(file="images/anneaux_olympiques.png").subsample(2)
 
+# Fonction pour afficher le menu de droite
+def menu_liste():
+    def liste_sportifs():
+        label_sportifs = tk.Label(cadre3, text="Liste des sportifs :", font=("Berlin Sans FB", 20), bg='lightgray', fg ='darkgreen')
+        label_sportifs.pack()
+    def liste_disciplines():
+        label_discipline = tk.Label(cadre3, text="Liste des disciplines :", font=("Berlin Sans FB", 20), bg='lightgray', fg ='darkgreen')
+        label_discipline.pack()
+    bouton_sportifs = tk.Button(cadre2,cursor='hand2', text="Sportifs", font=("Courrier", 12), width=12,bg='gray', fg='black', command=liste_sportifs)
+    bouton_sportifs.pack()
+    bouton_disciplines = tk.Button(cadre2,cursor='hand2', text="Disciplines", font=("Courrier", 12),width=12, bg='gray', fg='black', command=liste_disciplines)
+    bouton_disciplines.pack()
+    bouton_gerer_sportifs = tk.Button(cadre2,cursor='hand2', text="Gérer les sportifs", font=("Courrier", 10),width=12, bg='lightgreen', fg='black', command=test_liste)
+    bouton_gerer_sportifs.pack()
+
 # Fonction test pour futur affichage des listes
 def test_liste():
     def test_chercher():
         mot = "TA GUEULE"
         entry.delete(0, tk.END)
         entry.insert(0, mot)
-    entry = tk.Entry(cadre3, text="Jeux-Olympique", font=("Helvetica", 20), bg='lightgray', fg ='darkgreen')
+    entry = tk.Entry(cadre3, font=("Georgia", 20), bg='lightgray', fg ='darkgreen')
     entry.pack()
-    bouton_afficher2 = tk.Button(cadre3, text="Chercher", command=test_chercher, font=("Georgia", 12), bg='lightgreen', fg ='black')
+    bouton_afficher2 = tk.Button(cadre3, text="Chercher", command=test_chercher, font=("Georgia", 12), bg='lightgreen', fg ='black', cursor='hand2')
     bouton_afficher2.pack()
 
 # Fonction pour afficher la sélection
@@ -61,17 +76,12 @@ bouton_deroulant1 = tk.OptionMenu(cadre1, choix_selectionne1, *resultats)
 bouton_deroulant1.pack()
 
 # Bouton déroulant2
-bouton_deroulant2 = tk.OptionMenu(cadre2, choix_selectionne2, *listes)
-bouton_deroulant2.pack()
+#bouton_deroulant2 = tk.OptionMenu(cadre2, choix_selectionne2, *listes)
+#bouton_deroulant2.pack()
 
 # Bouton pour afficher les résultats
 bouton_afficher1 = tk.Button(cadre1, text="Résultats", command=lambda: afficher_selection(choix_selectionne1.get()))
 bouton_afficher1.pack()
-
-# Bouton pour afficher les différentes listes
-bouton_afficher2 = tk.Button(cadre2, text="Listes", command=test_liste)
-bouton_afficher2.pack()
-
 
 label_fond = tk.Label(fenetre, image=fond)
 label_fond.pack()
