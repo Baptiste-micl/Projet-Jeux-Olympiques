eeca396d42d0d7f70dcffa89f342d5963c64f7b4
Baptiste-micl <163674826+Baptiste-micl@users.noreply.github.com>
2024-03-25 09:13:48 +0100

    Update resultats.py

--- src/resultats.py
+++ src/resultats.py
@@ -1,1 +1,24 @@
+import tkinter as tk
+from mysql.connector import connect
 
+bdd = connect(host="localhost", user="root", password="auduse1306", database="Jeux_olympiques")
+
+class resultat_pays():
+    cursor = bdd.cursor()
+    cursor.execute("SELECT id_pays, medaille_or, medaille_argent, medaille_bronze FROM Resultats")
+    resultat = cursor.fetchall()
+
+    # Création d'un dictionnaire vide
+    dico_resultat_pays = {}
+
+resultat_pays = resultat_pays()
+
+class resultat_discipline():
+    cursor = bdd.cursor()
+    cursor.execute("SELECT id_pays, id_discipline, medaille_or, medaille_argent, medaille_bronze FROM Resultats")
+    resultat = cursor.fetchall()
+
+    # Création d'un dictionnaire vide
+    dico_resultat_disciplines = {}
+
+resultat_discipline = resultat_discipline()
