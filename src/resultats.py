d1acf9d43b292c79f85337557c9366905a6bc84c
Baptiste-micl <163674826+Baptiste-micl@users.noreply.github.com>
2024-04-18 11:14:03 +0200

    Update resultats.py

--- src/resultats.py
+++ src/resultats.py
@@ -5,10 +5,13 @@ bdd = connect(host="localhost", user="root", password="auduse1306", database="Je
 
 class resultat_pays():
     cursor = bdd.cursor()
-    cursor.execute("""SELECT Pays.nom_pays, medaille_or, medaille_argent, medaille_bronze 
-                   FROM Resultats 
-                   INNER JOIN Pays 
-                   ON Resultats.id_pays = Pays.id_pays
+    cursor.execute("""SELECT nom_pays AS Pays,
+                   FORMAT(SUM(medaille_or), 0) AS "Or",
+                   FORMAT(SUM(medaille_argent), 0) AS "Argent",
+                   FORMAT(SUM(medaille_bronze), 0) AS "Bronze"
+                   FROM Resultats
+                   JOIN Pays ON Pays.id_pays = Resultats.id_pays
+                   GROUP BY Pays.nom_pays
                    """)
     resultat = cursor.fetchall()
 
@@ -16,33 +19,34 @@ class resultat_pays():
     dico_resultat_pays = {}
 
     for resultat_pays in resultat:
-        nom_pays, medaille_or, medaille_argent, medaille_bronze = resultat_pays
+        Pays, Or, Argent, Bronze = resultat_pays
         # Ajout des valeurs au dictionnaire avec les clés 
-        dico_resultat_pays[nom_pays] = medaille_or, medaille_argent, medaille_bronze
+        dico_resultat_pays[Pays] = Or, Argent, Bronze
 
 resultat_pays = resultat_pays()
 
 class resultat_discipline():
     cursor = bdd.cursor()
-    cursor.execute(
-        """
-        SELECT Disciplines.nom_discipline, Pays.nom_pays, medaille_or, medaille_argent, medaille_bronze
-        FROM Resultats
-        INNER JOIN Pays ON Resultats.id_pays = Pays.id_pays
-        INNER JOIN Disciplines ON Resultats.id_discipline = Disciplines.id_discipline
-        ORDER BY Disciplines.nom_discipline
-        """
-    )
+    cursor.execute(""" SELECT nom_discipline AS Discipline,
+                   nom_pays AS Pays,
+                   FORMAT(SUM(medaille_or), 0) AS "Or",
+                   FORMAT(SUM(medaille_argent), 0) AS "Argent",
+                   FORMAT(SUM(medaille_bronze), 0) AS "Bronze"
+                   FROM Resultats
+                   JOIN Pays ON Pays.id_pays = Resultats.id_pays
+                   JOIN Disciplines ON Disciplines.id_discipline = Resultats.id_discipline
+                   GROUP BY Disciplines.nom_discipline, Pays.nom_pays
+                   ORDER BY Disciplines.nom_discipline, Pays.nom_pays;
+                   """)
     resultat = cursor.fetchall()
 
     # Création d'un dictionnaire vide
     dico_resultat_disciplines = {}
 
     for resultat_disciplines in resultat:
-        nom_discipline, nom_pays, medaille_or, medaille_argent, medaille_bronze = resultat_disciplines
-        # Ajout des valeurs au dictionnaire avec les clés 
-        dico_resultat_disciplines.setdefault(nom_discipline, []).append(
-            (nom_pays, medaille_or, medaille_argent, medaille_bronze)
+        Discipline, Pays, Or, Argent, Bronze = resultat_disciplines
+        dico_resultat_disciplines.setdefault(Discipline, []).append(
+            (Pays, Or, Argent, Bronze)
         )
     
 resultat_discipline = resultat_discipline()
