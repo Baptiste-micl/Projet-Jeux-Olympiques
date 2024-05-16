cbfc1659409b83e608bc1164f36c2c80543af2fb
Skymut <152205908+Skymut@users.noreply.github.com>
2024-05-16 09:00:06 +0200

    Update commentaires listes.py
    ajout commentaires

--- src/listes.py
+++ src/listes.py
@@ -1,37 +1,34 @@
 import tkinter as tk
 from mysql.connector import connect
 
-bdd = connect(host="localhost", user="root", password="auduse1306", database="Jeux_olympiques")
+# Connexion à la base de données MySQL
+bdd = connect(host="localhost", user="root", password="root", database="Jeux_olympiques")
 
+# Classe pour récupérer la liste des sportifs
 class liste_sportifs():
     cursor = bdd.cursor()
     cursor.execute("SELECT nom_sportif, prenom_sportif, id_pays FROM Sportifs")
     resultat = cursor.fetchall()
 
-    # Création d'un dictionnaire vide
+    # Création d'un dictionnaire pour stocker les sportifs
     dico_sportifs = {}
-
     for sportifs in resultat:
         nom, prenom, pays = sportifs
-        dico_sportifs.setdefault(pays, []).append((prenom, nom)) # changements
+        dico_sportifs.setdefault(pays, []).append((prenom, nom))
 
-liste_sportifs = liste_sportifs()
-
+# Classe pour récupérer la liste des sportifs
 class liste_disciplines():
     cursor = bdd.cursor()
     cursor.execute("SELECT nom_discipline, description_discipline FROM Disciplines")
     resultat = cursor.fetchall()
 
-    # Création d'un dictionnaire vide
+    # Création d'un dictionnaire pour stocker les disciplines et leurs descriptions
     dico_disciplines = {}
-
     for disciplines in resultat:
         nom_discipline, description_discipline = disciplines
-        # Ajout des valeurs au dictionnaire avec les clés "nom_discipline" et "description_discipline"
         dico_disciplines[nom_discipline] = description_discipline
 
-liste_disciplines = liste_disciplines() # ne sert à rien ?
-# Ici on ajoute sportif
+# Ici on ajoute le sportif à la base de données
 def ajouter_sportif(nom_sportif, prenom_sportif, id_pays, id_discipline):
     cursor = bdd.cursor()
     sql = "INSERT INTO Sportifs(nom_sportif, prenom_sportif, id_pays, id_discipline) VALUES (%s, %s, %s, %s)"
@@ -40,7 +37,7 @@ def ajouter_sportif(nom_sportif, prenom_sportif, id_pays, id_discipline):
     bdd.commit()
 
 
-# Ici on supprime un sportif
+# Ici on supprime un sportif de la base de données
 def supprimer_sportif(nom_sportif, prenom_sportif):
     cursor = bdd.cursor()
     sql = "DELETE FROM Sportifs WHERE nom_sportif = %s AND prenom_sportif = %s"
